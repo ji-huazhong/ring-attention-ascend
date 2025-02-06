913b7d1d8110e77ff70aa3a9485cfcf760c4f3fc
hzji210@gmail.com <hzji210@gmail.com>
2025-02-06 11:57:21 +0800

    fininsh zigzag_ring_flash_attn_forward

--- ring_attn_ascend/zigzag_ring_flash_attn.py
+++ ring_attn_ascend/zigzag_ring_flash_attn.py
@@ -13,16 +13,12 @@ from .utils import RingComm
 
 def _update_forward(
     prev_out: Optional[torch.Tensor],         # (batch_size, seqlen, nheads, d)
-    prev_softmax_max: Optional[torch.Tensor], # (batch_size, seqlen, nheads, 1)
-    prev_softmax_sum: Optional[torch.Tensor], # (batch_size, seqlen, nheads, 1)
+    prev_softmax_max: Optional[torch.Tensor], # (batch_size, nheads, seqlen, 8)
+    prev_softmax_sum: Optional[torch.Tensor], # (batch_size, nheads, seqlen, 8)
     cur_out: torch.Tensor, 
-    cur_softmax_max: torch.Tensor,            # (batch_size, nheads, seqlen, 8)
-    cur_softmax_sum: torch.Tensor,            # (batch_size, nheads, seqlen, 8)
+    cur_softmax_max: torch.Tensor, 
+    cur_softmax_sum: torch.Tensor,
 ) -> Tuple[torch.Tensor, torch.Tensor]:
-    
-    cur_softmax_max= cur_softmax_max[..., 0].transpose(-2, -1).unsqueeze(dim=-1) # b n s 8 -> b s n 1
-    cur_softmax_sum= cur_softmax_sum[..., 0].transpose(-2, -1).unsqueeze(dim=-1) # b n s 8 -> b s n 1
-    
     # update softmax max
     softmax_max = torch.maximum(prev_softmax_max, cur_softmax_max)
     
@@ -37,15 +33,12 @@ def _update_forward(
     prev_out_scale = prev_softmax_sum_scaled / softmax_sum
     cur_out_scale = cur_softmax_sum_scaled / softmax_sum
 
-    # #[b, n, s, 8] -> [b, s, n, d]
-    # [b, s, n, 1] -> [b, s, n, d]
+    # [b, n, s, 8] -> [b, s, n, d]
     d = cur_out.shape[-1]
-    # prev_out_scale = prev_out_scale[..., 0].unsqueeze(3).repeat(1, 1, 1, d) # [b, n, s, 1] -> [b, n, s, d]
-    # prev_out_scale = rearrange(prev_out_scale, "b n s d -> b s n d").contiguous()
-    # cur_out_scale = cur_out_scale[..., 0].unsqueeze(3).repeat(1, 1, 1, d)
-    # cur_out_scale = rearrange(cur_out_scale, "b n s d -> b s n d").contiguous()
-    prev_out_scale = prev_out_scale.repeat(1, 1, 1, d).contiguous()
-    cur_out_scale = cur_out_scale.repeat(1, 1, 1, d).contiguous()
+    prev_out_scale = prev_out_scale[..., 0].unsqueeze(3).repeat(1, 1, 1, d) # [b, n, s, 1] -> [b, n, s, d]
+    prev_out_scale = rearrange(prev_out_scale, "b n s d -> b s n d").contiguous()
+    cur_out_scale = cur_out_scale[..., 0].unsqueeze(3).repeat(1, 1, 1, d)
+    cur_out_scale = rearrange(cur_out_scale, "b n s d -> b s n d").contiguous()
 
     # updata output
     out = prev_out * prev_out_scale + cur_out * cur_out_scale
@@ -59,20 +52,11 @@ def update_forward(
     block_out: torch.Tensor, 
     block_mqk: torch.Tensor, 
     block_se: torch.Tensor,
-    slice_=None,
 ) -> Tuple[torch.Tensor, torch.Tensor]:
     if out is None:
-        if slice_ is not None:
-            raise RuntimeError("first update_forward should not pass slice_ args")
         out = block_out.to(torch.float32)
-        mqk = block_mqk[..., 0].transpose(-2, -1).unsqueeze(dim=-1) # b n s 8 -> b s n 1
-        se = block_se[..., 0].transpose(-2, -1).unsqueeze(dim=-1)
-    elif slice_ is not None:
-        # 请注意mqk se的shape跟out是不一致的！
-        slice_out, slice_mqk, slice_se = out[slice_], mqk[slice_], se[slice_]
-        slice_out, slice_mqk, slice_se = _update_forward(
-            slice_out, slice_mqk, slice_se, block_out, block_mqk, block_se
-        )
+        mqk = block_mqk
+        se = block_se
     else:
         out, mqk, se = _update_forward(out, mqk, se, block_out, block_mqk, block_se)
     return out, mqk, se
@@ -128,116 +112,34 @@ def zigzag_ring_flash_attn_forward(
             out, mqk, se = update_forward(out, mqk, se, block_out, block_mqk, block_se)
         else:
             block_out, block_mqk, block_se = forward(q1, k, v, causal=False)
-            if comm.rank == 0:
-                print(f">>>>{block_out.shape} {block_mqk.shape} {out.shape} {mqk.shape}")
-            out, mqk, se = update_forward(
-                out, 
-                mqk, 
-                se,
+            # [b, s, n, d] -> [b, 2, s//2, n, d]
+            out = out.view(out.shape[0], 2, out.shape[1]//2, out.shape[2], out.shape[-1])
+            # [b, n, s, 8] -> [b, n, 2, s//2, 8]
+            mqk = mqk.view(mqk.shape[0], mqk.shape[1], 2, mqk.shape[2]//2, mqk.shape[-1])
+            se = se.view(se.shape[0], se.shape[1], 2, se.shape[2]//2, se.shape[-1])
+            updated_out, updated_mqk, updated_se = update_forward(
+                out[:, 1], 
+                mqk[:, :, 1], 
+                se[:, :, 1],
                 block_out,
                 block_mqk,
                 block_se,
-                slice_=(slice(None), slice(block_seq_len, None)), # [:, block_seq_len: ]
             )
+            out[:, 1].copy_(updated_out)
+            mqk[:, :, 1].copy_(updated_mqk)
+            se[:, :, 1].copy_(updated_se)
+            # [b, 2, s//2, n, d] -> [b, s, n, d]
+            out = out.view(out.shape[0], -1, out.shape[-2], out.shape[-1])
+            # [b, n, 2, s//2, 8] -> [b, n, s, 8]
+            mqk = mqk.view(mqk.shape[0], mqk.shape[1], -1, mqk.shape[-1])
+            se = se.view(se.shape[0], se.shape[1], -1, se.shape[-1])
 
         if step + 1 != comm.world_size:
             comm.wait()
             k, v = next_k, next_v
     
     out = out.to(q.dtype)
-    mqk = mqk.squeeze(dim=-1).transpose(1, 2).unsqueeze(dim=-1).repeat(1, 1, 1, 8)
-    se = se.squeeze(dim=-1).transpose(1, 2).unsqueeze(dim=-1).repeat(1, 1, 1, 8)
     return out, mqk, se
-
-
-def zigzag_ring_flash_attn_backward(
-    process_group,
-    dout,
-    q, # [bsz, seqlen, nheads, headdim]
-    k,
-    v,
-    out,
-    softmax_scale,
-    attn_mask,
-    softmax_max,
-    softmax_sum,
-    causal=True,
-):
-    assert causal is True, "zigzag ring is meaningless for causal=False"
-    kv_comm = RingComm(process_group)
-    d_kv_comm = RingComm(process_group)
-    dq, dk, dv = None, None, None
-    next_dk, next_dv = None, None
-    next_k, next_v = None, None
-    dk_comm_buffer, dv_comm_buffer = None, None
-
-    dout1 = dout.chunk(2, dim=1)[1]
-    q1 = q.chunk(2, dim=1)[1]
-    out1 = out.chunk(2, dim=1)[1]
-    softmax_max1 = softmax_max.chunk(2, dim=2)[1].contiguous()
-    softmax_sum1 = softmax_sum.chunk(2, dim=2)[1].contiguous()
-    block_seq_len = q.shape[1] // 2
-
-    def backward(dout, q, k, v, out, softmax_max, softmax_sum, causal):
-        seqlen_q = q.shape[1]
-        seqlen_kv = k.shape[1]
-        attn_grad_outs = torch_npu.npu_fusion_attention_grad(
-            q,
-            k,
-            v,
-            dout,
-            head_num=q.shape[2],
-            input_layout="BSND",
-            atten_mask=attn_mask if causal else None,
-            softmax_max=softmax_max,
-            softmax_sum=softmax_sum,
-            attention_in=out,
-            scale_value=softmax_scale,
-            pre_tockens=k.shape[1],
-            next_tockens=0,
-        )
-        return attn_grad_outs
-    
-    for step in range(kv_comm.world_size):
-        if step + 1 != kv_comm.world_size:
-            next_k, next_v = kv_comm.send_recv_kv(k, v)
-        
-        if step == 0:
-            grad_outs = backward(dout, q, k, v, out, softmax_max, softmax_sum, causal=True)
-            dq = grad_outs[0].to(torch.float32)
-            dk = grad_outs[1].to(torch.float32)
-            dv = grad_outs[2].to(torch.float32)
-        else:
-            if step <= kv_comm.rank:
-                k0 = k[:, :block_seq_len]
-                v0 = v[:, :block_seq_len]
-                grad_outs = backward(dout, q, k0, v0, out, softmax_max, softmax_sum, causal=False)
-                dq += grad_outs[0]
-            else:
-                grad_outs = backward(dout1, q1, k, v, out1, softmax_max1, softmax_sum1, causal=False)
-                dq[:, block_seq_len:] += grad_outs[0][:, :block_seq_len]
-
-            d_kv_comm.wait()
-            dk_comm_buffer, dv_comm_buffer = dk, dv
-            dk, dv = next_dk, next_dv
-
-            if step <= kv_comm.rank:
-                dk[:, :block_seq_len] += grad_outs[1][:, :block_seq_len]
-                dv[:, :block_seq_len] += grad_outs[2][:, :block_seq_len]
-            else:
-                dk += grad_outs[1]
-                dv += grad_outs[2]
-
-        if step + 1 != kv_comm.world_size:
-            kv_comm.wait()
-            k, v = next_k, next_v
-
-        next_dk, next_dv = d_kv_comm.send_recv_kv(
-            dk, dv, dk_comm_buffer, dv_comm_buffer
-        ) # TODO
-    d_kv_comm.wait()
-
-    return dq.to(q.dtype), next_dk.to(q.dtype), next_dv.to(q.dtype)
 
 
 class ZigZagRingFlashAttnFunc(torch.autograd.Function):
@@ -279,21 +181,7 @@ class ZigZagRingFlashAttnFunc(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, dout, *args):
-        q, k, v, out, softmax_max, softmax_sum = ctx.saved_tensors
-        dq, dk, dv = zigzag_ring_flash_attn_backward(
-            ctx.group,
-            dout,
-            q,
-            k,
-            v,
-            out,
-            softmax_scale=ctx.softmax_scale,
-            attn_mask=ctx.attn_mask,
-            softmax_max=softmax_max,
-            softmax_sum=softmax_sum,
-        )
-        return dq, dk, dv, None, None, None, None
-
+        ...
 
 def zigzag_ring_flash_attn_func(
     q,
