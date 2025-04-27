8f567bfd9ec5365913ce5395b955241cf9d16d98
zhuo97 <1103045176@qq.com>
2025-04-27 15:00:47 +0800

    add hf adapter and all gather func

--- ring_attn_ascend/__init__.py
+++ ring_attn_ascend/__init__.py
@@ -7,3 +7,7 @@ from .ring_flash_attn_varlen import ring_flash_attn_varlen_func
 from .zigzag_ring_flash_attn import zigzag_ring_flash_attn_func
 from .zigzag_ring_flash_attn_varlen import zigzag_ring_flash_attn_varlen_func
 from .utils import flatten_softmax, unflatten_softmax, get_sub_seq_lens 
+from .adapters import (
+    substitute_hf_flash_attn,
+    update_ring_flash_attn_params,
+)
