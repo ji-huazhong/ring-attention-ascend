4a7463bf28560644d58f26ac61ef968353324da8
Huazhong Ji <hzji210@gmail.com>
2025-05-24 11:41:33 +0800

    v0.0.2 (#6)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ring_attn_ascend",
-    version="0.1",
+    version="0.0.2",
     author="jihuazhong",
     url="https://github.com/ji-huazhong/ring-attention-ascend",
     packages=find_packages(),
